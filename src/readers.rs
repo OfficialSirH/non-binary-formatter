a957643834c8e2ec91728e5b3b0c3a3a6edec13b
Micah Benac <micahbenac52@gmail.com>
2024-08-26 12:12:32 -0400

    chore(readers): use impl trait instead of generic trait

--- src/readers.rs
+++ src/readers.rs
@@ -7,8 +7,8 @@ use crate::errors::NrbfError;
 /// Will read the amount of bytes necessary for any type that implements [`FromBytes`]
 ///
 /// [`read_bytes`] has an overloaded return type. If you do not specify the return type it may produce a surprising type to satisfy inference.
-pub fn read_bytes<const N: usize, T: FromBytes<Bytes = [u8; N]>, R: Read>(
-    reader: &mut R,
+pub fn read_bytes<const N: usize, T: FromBytes<Bytes = [u8; N]>>(
+    reader: &mut impl Read,
 ) -> Result<T, NrbfError> {
     let mut buffer = [0u8; N];
     reader.read_exact(&mut buffer)?;
