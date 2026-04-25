747ac637946fc6868421002930a1adea73010452
micahbenac52@gmail.com <micahbenac52@gmail.com>
2026-04-25 20:34:21 +0000

    chore: get nix build working

--- flake.nix
+++ flake.nix
@@ -1,50 +1,77 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, fenix }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+    }:
     let
-      systems = ["x86_64-linux" "aarch64-linux"];
-      
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs systems (system:
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
           f {
-            pkgs =
-              import nixpkgs {
-                inherit system;
-                overlays = [self.overlays.default];
-              };
-          });
-  
-    in {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          }
+        );
+
+    in
+    {
       overlays.default = final: prev: {
-			rustToolchain = with fenix.packages.${prev.stdenv.hostPlatform.system};
-				combine (
-					(with stable; [clippy rustc cargo rust-src rust-analyzer])
-					++ [default.rustfmt]
-				);
-    	};
-		      
-      packages =
-      			forAllSystems ({pkgs}: {
-      					default =
-      						(pkgs.makeRustPlatform {
-      								cargo = pkgs.rustToolchain;
-      								rustc = pkgs.rustToolchain;
-      							}).buildRustPackage {
-      							pname = "non-binary-formatter";
-      							version = "0.0.1";
-      							src = ./.;
-      							cargoLock.lockFile = ./Cargo.lock;
-      						};
-      				});
-      				      
-			devShells = forAllSystems (
+        rustToolchain =
+          with fenix.packages.${prev.stdenv.hostPlatform.system};
+          combine (
+            (with latest; [
+              clippy
+              rustc
+              cargo
+              rust-src
+              rust-analyzer
+            ])
+            ++ [ latest.rustfmt ]
+          );
+      };
+
+      packages = forAllSystems (
+        { pkgs }:
+        {
+          default =
+            (pkgs.makeRustPlatform {
+              cargo = pkgs.rustToolchain;
+              rustc = pkgs.rustToolchain;
+            }).buildRustPackage
+              {
+                pname = "non-binary-formatter";
+                version = "0.0.1";
+                src = ./.;
+                cargoLock.lockFile = ./Cargo.lock;
+
+                buildInputs = with pkgs; [
+                  gcc.cc.lib
+                ];
+
+                LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (with pkgs; [ gcc.cc.lib ])}:$LD_LIBRARY_PATH";
+              };
+        }
+      );
+
+      devShells = forAllSystems (
         { pkgs }:
         {
           default = pkgs.mkShell {
@@ -57,17 +84,16 @@
               cargo-watch
               cargo-show-asm
               bacon
-            ];
+];
 
             buildInputs = with pkgs; [
               gcc.cc.lib
             ];
 
-            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (with pkgs; [gcc.cc.lib])}:$LD_LIBRARY_PATH";
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (with pkgs; [ gcc.cc.lib ])}:$LD_LIBRARY_PATH";
             RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
           };
         }
       );
-
     };
 }
